da34913dbdfeb3985842432a0455345928534b51
Bill Reed <billreed72@gmail.com>
2013-12-29 14:47:21 -0600

    Update fullAccess.ps1

--- fullAccess.ps1
+++ fullAccess.ps1
@@ -1,13 +1,13 @@
-Write-Host "Mailbox			Full Access Granted" -ForegroundColor Magenta
-Write-Host "-------			-------------------" -ForegroundColor Green
+Write-Host 'Mailbox___Full Access Granted' -ForegroundColor Magenta
+Write-Host '-----------------------------' -ForegroundColor Green
 Import-CSV 'c:\userList-ADID-SMTP.csv' |
 ForEach {
 $Email = $_.PrimarySmtpAddress
 $sam = $_.Identity
 ########################################
-$FullAccess = Get-MailboxPermission -Identity $Email | where { ($_.AccessRights -eq "FullAccess" -and $_.IsInherited -eq $false -and $_.User -notlike "NT AUTHORITY\SELF") } | Select User
+$FullAccess = Get-MailboxPermission -Identity $Email | Where { ($_.AccessRights -eq 'FullAccess' -and $_.IsInherited -eq $False -and $_.User -notlike 'NT AUTHORITY\SELF') } | Select User
 Foreach ($User in $FullAccess)
  {
-  Write-Host $Email -ForegroundColor Magenta -NoNewLine; Write-Host " - " $User.User  -ForegroundColor Cyan
+  Write-Host $Email -ForegroundColor Magenta -NoNewLine; Write-Host ' - ' $User.User  -ForegroundColor Cyan
  }
 }
