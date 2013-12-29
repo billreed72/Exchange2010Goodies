5da5c1886fd32da1a9381fa7bb2e4e8bc8b086d1
Bill Reed <billreed72@gmail.com>
2013-12-29 15:52:18 -0600

    Create MAPIPermissions.ps1

---
+++ MAPIPermissions.ps1
@@ -0,0 +1,24 @@
+Write-Host 'Mailbox MAPI Permissions' -ForegroundColor Magenta
+Write-Host '------------------------' -ForegroundColor Green
+Import-CSV 'c:\userList-ADID-SMTP.csv' |
+foreach {
+$Email = $_.PrimarySmtpAddress
+$sam = $_.Identity
+
+########## MAILBOX FOLDERS ############
+$folders = Get-MailboxFolderStatistics -Identity $Email | Where {$_.Foldertype -ne "SyncIssues" -and $_.Foldertype -ne "Conflicts" -and $_.Foldertype -ne "LocalFailures" -and $_.Foldertype -ne "ServerFailures" -and $_.Foldertype -ne "RecoverableItemsRoot" -and $_.Foldertype -ne "RecoverableItemsDeletions" -and $_.Foldertype -ne "RecoverableItemsPurges" -and $_.Foldertype -ne "RecoverableItemsVersions" -and $_.Foldertype -ne "Root"} | select folderpath,ItemsInFolder,FolderSize
+#Write-Host "Mailbox Folders:" -ForegroundColor Green
+Foreach ($Folder in $folders)
+ {
+#  Write-Host 'Folder:'$Folder.Folderpath 'Items:'$Folder.ItemsInFolder 'Size:'$Folder.FolderSize
+ }
+########## MAPI PERMISSIONS ###########
+Write-Host "MAPI Permissions: " -ForegroundColor Green -NoNewLine; Write-Host $Email -ForegroundColor Yellow -BackgroundColor DarkBlue
+get-mailboxfolderpermission -identity $Email":\" | ft foldername, User, AccessRights
+Foreach ($Folder in $folders)
+ {
+  $NormalizedFolder = $Folder.FolderPath.Replace("/","\")
+  $NormalizedIdentity = $Email + ":" + $NormalizedFolder
+  get-mailboxfolderpermission -identity $NormalizedIdentity | ft foldername, User, AccessRights
+ }
+}
