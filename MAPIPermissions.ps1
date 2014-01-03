69298e9c5a8d8e16deb02c6353d6fd80f63a9047
Bill Reed <billreed72@gmail.com>
2014-01-03 11:14:26 -0500

    Update MAPIPermissions.ps1

--- MAPIPermissions.ps1
+++ MAPIPermissions.ps1
@@ -1,10 +1,12 @@
 Write-Host 'Mailbox MAPI Permissions' -ForegroundColor Magenta
 Write-Host '------------------------' -ForegroundColor Green
 Import-CSV 'c:\userList-ADID-SMTP.csv' |
+# The CSV file contains 2 columns of data:
+# "PrimarySmtpAddress","Identity"
+# "btester@dex10.net","dex10.net/Staff/Bob Tester"
 foreach {
 $Email = $_.PrimarySmtpAddress
 $sam = $_.Identity
-
 ########## MAILBOX FOLDERS ############
 $folders = Get-MailboxFolderStatistics -Identity $Email | Where {$_.Foldertype -ne "SyncIssues" -and $_.Foldertype -ne "Conflicts" -and $_.Foldertype -ne "LocalFailures" -and $_.Foldertype -ne "ServerFailures" -and $_.Foldertype -ne "RecoverableItemsRoot" -and $_.Foldertype -ne "RecoverableItemsDeletions" -and $_.Foldertype -ne "RecoverableItemsPurges" -and $_.Foldertype -ne "RecoverableItemsVersions" -and $_.Foldertype -ne "Root"} | select folderpath,ItemsInFolder,FolderSize
 #Write-Host "Mailbox Folders:" -ForegroundColor Green
