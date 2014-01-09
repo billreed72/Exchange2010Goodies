739ba99019d4653c0c51791e11c8e02fcf73ff8a
Bill Reed <billreed72@gmail.com>
2014-01-09 13:20:36 -0500

    Update WorkingArea.ps1

--- WorkingArea.ps1
+++ WorkingArea.ps1
@@ -16,3 +16,33 @@ Foreach ($User in $SendOnBehalf)
   Write-Host $Email -ForegroundColor Magenta -NoNewLine; Write-Host " - " $User.SendOnBehalf  -ForegroundColor Cyan
  }
 }
+
+param([string]$UserListFile=$(throw "User List needed!"))
+$OutData = @()
+$OutFile = "SharedMailboxDia-Deleg.csv"
+$UserList = get-content $UserListFile
+$CurProcMbx = 1
+
+Foreach ($UserID in $UserList) {
+  write-progress "Processing Mailbox num: " $CurProcMbx
+  $FinalList = @()
+  $User = Get-mailbox $UserID
+  $InitialList = $User.GrantSendOnBehalfTo
+  Foreach ($recipient in $InitialList) {
+    $type = (Get-recipient $recipient.Name).RecipientType
+    If ($type -like "*group*") {$FinalList += RecExpand ($recipient.Name)}
+    Else {$FinalList += $recipient}
+  }
+  Foreach ($recipient in $FinalList) {
+    If ($recipient -ne $NULL) {
+      $OutObject = "" | select Mailbox, Delegate
+      $OutObject.Mailbox = $User.PrimarySmtpAddress
+      $OutObject.Delegate = (Get-Recipient $recipient).PrimarySmtpAddress.ToString()
+      $Outdata += $OutObject
+      $OutObject
+    }
+  }
+  $CurProcMbx++
+}
+
+$OutData | export-csv $Outfile
