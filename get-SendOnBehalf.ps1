f1e1df24c87797f8a53747ad3dc70d4eb557281c
Bill Reed <billreed72@gmail.com>
2014-01-09 14:53:06 -0500

    Update and rename WorkingArea.ps1 to get-SendOnBehalf.ps1

---
+++ get-SendOnBehalf.ps1
@@ -0,0 +1,36 @@
+Write-Host '----------------------------------------'
+Write-Host 'Please provide an INPUT filename.' -ForegroundColor Cyan -BackgroundColor DarkBlue;
+$UserListFile = Read-Host '(i.e. c:\userList.csv or userList.csv)';
+Write-Host '----------------------------------------'
+Write-Host 'And now... provide an OUTPUT filename.' -ForegroundColor White -BackgroundColor DarkGreen;
+$OutFile = Read-Host '(i.e. c:\outputfile.csv or outputfile.csv)'
+$OutData = @()
+
+$UserList = get-content $UserListFile
+$CurProcMbx = 1
+
+Foreach ($UserID in $UserList) {
+  write-progress "Processing Mailbox num: " $CurProcMbx
+  $FinalList = @()
+  $User = Get-mailbox $UserID
+  $InitialList = $User.GrantSendOnBehalfTo
+  Foreach ($recipient in $InitialList) {
+    $type = (Get-recipient $recipient.Name).RecipientType
+    If ($type -like "*group*") {$FinalList += RecExpand ($recipient.Name)}
+    Else {$FinalList += $recipient}
+  }
+  Foreach ($recipient in $FinalList) {
+    If ($recipient -ne $NULL) {
+      $OutObject = "" | select Mailbox, Delegate
+      $OutObject.Mailbox = $User.PrimarySmtpAddress
+      $OutObject.Delegate = (Get-Recipient $recipient).PrimarySmtpAddress.ToString()
+      $Outdata += $OutObject
+      $OutObject
+    }
+  }
+  $CurProcMbx++
+}
+$OutData | export-csv $Outfile;
+Write-Host '----------------------------------------' -ForegroundColor Green;
+Write-Host 'All done! Have a nice day. :-) ' -ForegroundColor Magenta;
+Write-Host '----------------------------------------' -ForegroundColor Green
