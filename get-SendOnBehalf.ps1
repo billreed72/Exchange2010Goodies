0af459cbe635939635613c5cf06137bc293ef0f2
Bill Reed <billreed72@gmail.com>
2014-01-12 09:22:11 -0500

    Update get-SendOnBehalf.ps1

--- get-SendOnBehalf.ps1
+++ get-SendOnBehalf.ps1
@@ -1,36 +1,41 @@
 Write-Host 'INPUT filename:' -ForegroundColor Cyan -BackgroundColor DarkBlue;
 $UserListFile = Read-Host '(i.e. c:\userList.csv or userList.csv)';
 $OutData = @()
+$UserList = get-content $UserListFile
+$CurProcMbx = 1
+###### Function 1: Need to gain understanding
 Function RecExpand ($grpn) {
 $grpfinal= @()
 $grp = Get-DistributionGroupMember -Identity $grpn -ResultSize unlimited
+###### Loop w/in Function: Expand group members into array
 Foreach ($g in $grp) {
   if($g.RecipientType -like "*group*"){$grpfinal += RecExpand $g.Tostring()}
   else{$grpfinal += $g.Tostring()}
  }
  Return $grpfinal
 }
-$UserList = get-content $UserListFile
-$CurProcMbx = 1
+###### Loop 1: Get the UserID, save to array
 Foreach ($UserID in $UserList) {
   write-progress "Processing Mailbox num: " $CurProcMbx
   $FinalList = @()
   $User = Get-mailbox $UserID
   $InitialList = $User.GrantSendOnBehalfTo
-  Foreach ($recipient in $InitialList) {
+###### Loop 2: Query for groups to expand, otherwise, save recipients to array
+Foreach ($recipient in $InitialList) {
     $type = (Get-recipient $recipient.Name).RecipientType
     If ($type -like "*group*") {$FinalList += RecExpand ($recipient.Name)}
     Else {$FinalList += $recipient}
   }
+###### Loop 3: 
   Foreach ($recipient in $FinalList) {
     If ($recipient -ne $NULL) {
-      $OutObject = "" | select Mailbox, Delegate
+      $OutObject = "" | select Mailbox, SendOnBehalfAccess
       $OutObject.Mailbox = $User.PrimarySmtpAddress
-      $OutObject.Delegate = (Get-Recipient $recipient).PrimarySmtpAddress.ToString()
+      $OutObject.SendOnBehalfAccess = (Get-Recipient $recipient).PrimarySmtpAddress.ToString()
       $Outdata += $OutObject
       $OutObject
     }
   }
   $CurProcMbx++
 }
-$Outdata | Export-csv  -Path ('SendOnBehalfOutput-{1:yyyyMMddHHmm}.csv' -f $env:COMPUTERNAME,(Get-Date));
+$Outdata | Export-csv  -Path ('SendOnBehalf-{1:yyyyMMddHHmm}.csv' -f $env:COMPUTERNAME,(Get-Date));
