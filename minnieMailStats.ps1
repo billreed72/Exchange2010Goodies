32f19c499e364f18e2e03b502227c20308e508cf
Bill Reed <billreed72@gmail.com>
2014-02-03 22:02:46 -0500

    Update minnieMailStats.ps1

--- minnieMailStats.ps1
+++ minnieMailStats.ps1
@@ -1,10 +1,33 @@
-function msgstats {
-    $startDate = read-host 'Start Date [mm/dd/yyyy]'
-    $serverFQDN = read-host 'Server FQDN [dex.dev10.net]'
-    get-messagetrackinglog -Server $serverFQDN -start $startDate -resultsize unlimited | where { ($_.eventID -eq 'DELIVER') -and ($_.source -eq 'STOREDRIVER') } | %{$count++}
-    write-host '============================' -fore yellow -back darkBlue
-    write-host 'Total Messages: ' -fore yellow -back darkBlue -NoNewLine; write-host "..." $count "..." -fore blue -back white
-    write-host '============================' -fore yellow -back darkBlue
+#
+# create a local event log named emailstats
+# Example: new-Eventlog -LogName "emailstats" -Source "emailstatsSource"
+$hts = get-exchangeserver |? {$_.serverrole -match "hubtransport"} |% {$_.name}
+$startdate = "01/27/2014 12:01:01 AM"
+$enddate = "02/02/2014 11:59:50 PM"
+
+function time_pipeline {
+param ($increment  = 1000)
+begin{$i=0;$timer = [diagnostics.stopwatch]::startnew()}
+process {
+    $i++
+    if (!($i % $increment)){Write-host “`rProcessed $i in $($timer.elapsed.totalseconds) seconds” -nonewline}
+    $_
+    }
+end {
+	write-host “`rProcessed $i log records in $($timer.elapsed.totalseconds) seconds”
+	Write-Host "   Average rate: $([int]($i/$timer.elapsed.totalseconds)) log recs/sec."
+	}
 }
-$count = 0
-msgstats
+
+function miniemailstats {
+get-messagetrackinglog -Server $ht -EventID "DELIVER" -Start $startdate -End $enddate -resultsize unlimited | time_pipeline | %{$count++}
+write-host "Server $ht Message count " $count
+write-EventLog -LogName "emailstats" -EventID 666 -Message "Email Server $ht Stat Count $count number of 
+
+messages between dates $startdate and $enddate " -Source "emailstats" -EntryType Information
+}
+
+foreach ($ht in $hts){
+$count=0
+miniemailstats
+}
